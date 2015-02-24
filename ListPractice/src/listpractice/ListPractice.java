7c5f45b49de11d8f7e2437a685166b4c37d37a22
MVargas <MVargas@10.1.213.10>
2015-02-24 10:38:28 -0500

    ListPratice - tweaked

--- ListPractice/src/listpractice/ListPractice.java
+++ ListPractice/src/listpractice/ListPractice.java
@@ -25,18 +25,27 @@ public class ListPractice {
         stringList.add("Curly");
 
         System.out.println(stringList.size());
+        System.out.print("______________________________");
         System.out.println(stringList.get(0));
+        System.out.print("______________________________");
         System.out.println(stringList.get(1));
 
         stringList.set(0, "John");
         stringList.remove(1);
-
+        
+        System.out.print("______________________________");
+        
         for (int i = 0; i < stringList.size(); i++) {
             System.out.println(stringList.get(i));
         }
+        
+        System.out.print("______________________________");
+        
         for (String name : stringList) {
             System.out.println(name);
         }
+        
+        System.out.print("______________________________");
         //todo: extra credit, two other forms to print list
     }
 
